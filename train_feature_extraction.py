4b84161ac9f646a93c300b7101d0475eed89235c
Michael J. Kitchin <michael.kitchin@opsysinc.com>
2018-01-23 16:09:25 -0800

    Updated.

--- train_feature_extraction.py
+++ train_feature_extraction.py
@@ -45,9 +45,9 @@ keep_probability = 0.5
 learning_rate = 0.0013
 
 # x = tf.placeholder(tf.float32, (None, 32, 32, 3))
-x_tensor = tf.placeholder(tf.float32, (None, 32, 32, 3))
-y_tensor = tf.placeholder(tf.int64, None)
-resized = tf.image.resize_images(x_tensor, (227, 227))
+images_tensor = tf.placeholder(tf.float32, (None, 32, 32, 3))
+labels_tensor = tf.placeholder(tf.int64, None)
+resized = tf.image.resize_images(images_tensor, (227, 227))
 keep = tf.placeholder_with_default(1.0, shape=None)
 
 # TODO: pass placeholder as first argument to `AlexNet`.
@@ -56,31 +56,33 @@ fc7 = AlexNet(resized, feature_extract=True)
 # past this point, keeping the weights before and up to `fc7` frozen.
 # This also makes training faster, less work to do!
 fc7 = tf.stop_gradient(fc7)
-fc_dropout = tf.nn.dropout(fc7, keep)
+fc7_dropout = tf.nn.dropout(fc7, keep)
 
 # TODO: Add the final layer for traffic sign classification.
-shape = (fc_dropout.get_shape().as_list()[-1], nb_classes)  # use this shape for the weight matrix
-fc_new_1 = tf.Variable(tf.truncated_normal(shape=shape, mean = 0.0, stddev = 0.1))
-fc_new_2 = tf.Variable(tf.zeros(nb_classes))
-logits = tf.add(tf.matmul(fc_dropout, fc_new_1),fc_new_2)
+fc7_shape = (fc7_dropout.get_shape().as_list()[-1], nb_classes)  # use this shape for the weight matrix
+fc_new_W = tf.Variable(tf.truncated_normal(shape=fc7_shape, mean = 0.0, stddev = 0.1))
+fc_new_b = tf.Variable(tf.zeros(nb_classes))
+logits = tf.nn.xw_plus_b(fc7_dropout, fc_new_W, fc_new_b)
 
 # TODO: Define loss, training, accuracy operations.
 # HINT: Look back at your traffic signs project solution, you may
 # be able to reuse some the code.
-cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=y_tensor,logits=logits)
-loss_operation = tf.reduce_mean(cross_entropy)
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+cross_entropy_operation = tf.nn.softmax_cross_entropy_with_logits(labels=labels_tensor, logits=logits)
+loss_operation = tf.reduce_mean(cross_entropy_operation)
+optimizer_operation = tf.train.AdamOptimizer(learning_rate=learning_rate)
 
-training_operation = optimizer.minimize(loss_operation)
+training_operation = optimizer_operation.minimize(loss_operation, var_list=[fc_new_W, fc_new_b])
+init_operation = tf.initialize_all_variables()
+
 prediction_operation = tf.argmax(logits, 1)
-accuracy_operation = tf.reduce_mean(tf.cast(tf.equal(prediction_operation, y_tensor), tf.float32))
+accuracy_operation = tf.reduce_mean(tf.cast(tf.equal(prediction_operation, labels_tensor), tf.float32))
 
 # TODO: Train and evaluate the feature extraction model.
 num_examples = len(X_train)
-
 with tf.Session() as sess:
+    sess.run(init_operation)
     for offset in range(0,num_examples,batch_size):
         end = offset+batch_size
         batch_x, batch_y = X_train[offset:end],y_train[offset:end]
         sess.run(training_operation,
-                 feed_dict={x_tensor:batch_x,y_tensor:batch_y,keep:keep_probability})
+                 feed_dict={images_tensor:batch_x, labels_tensor:batch_y, keep:keep_probability})
